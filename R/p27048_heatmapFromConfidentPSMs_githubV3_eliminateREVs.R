44348ace3562408716bd4e7fafe00b3502f27fc2
ShevanAncientProteins <shevanwilkin@me.com>
2024-08-23 18:48:07 +0200

    Added a line to make a pdf for myself for 10 psms

--- R/p27048_heatmapFromConfidentPSMs_githubV3_eliminateREVs.R
+++ R/p27048_heatmapFromConfidentPSMs_githubV3_eliminateREVs.R
@@ -348,6 +348,7 @@ filtMat_min10 <- newFiltMat[bool_keep_10,]
 # 
 rscol2 <- row_side_colors_matrix[bool_keep_10, ]
 (fN <- paste(fgczProject, "_", descri, "_noBlanks_min10psms", ".pdf",sep = ""))
+pdf("p27048_heatmap_HUMANonly_betterColors_noBlanks_min10psms.pdf", 25,25)
 pdf(fN, 31,31)
 # Create the heatmap with row side colors list
 heatmap.plus(
