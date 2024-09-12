b0143f0f09e06647844c905642b5cf31994520c8
jjGG <jonas.grossmann@uzh.ch>
2024-09-12 14:50:05 +0200

    hunt and replace strange varmod search

--- R/p27048_LookAtDeam.R
+++ R/p27048_LookAtDeam.R
@@ -26,8 +26,15 @@ table(datOK$pep_var_mod)
 # LTNNRYSDIDSIALIDK Y 2 Deamidated (NQ) 0.00110000000000000.0
 # Deamidation == 1
 
+table(is.na(datOK$pep_var_mod_pos))
 datOK$DeamidationBool <- str_count(datOK$pep_var_mod_pos, pattern = "1") > 0
-datOK$DeamidationBool[datOK$DeamidationBool == NA] <- FALSE
+datOK$DeamidationBool[is.na(datOK$DeamidationBool)] <- FALSE
+table(datOK$DeamidationBool)
+#
+datOK$pep_var_mod[datOK$DeamidationBool]
+
+table(datOK$pep_var_mod[datOK$DeamidationBool])
+
 # text table
 table(datOK$DeamidationBool, datOK$rawFile)
 
@@ -43,3 +50,18 @@ pdf("Deamidation_yes_no_nonstacked.pdf", 19,28)
 par(mar = c(4.1, 14.4, 4.1, 1.9))
 barplot(table(datOK$DeamidationBool, datOK$rawFile), horiz = TRUE, las=2, margin = 1, beside = TRUE, col = c("red", "blue"), main = "Deamidation", xlab = "Count", ylab = "RawFile")
 dev.off()
+
+
+# hunt the methyl
+datOK$MethylBool <- str_count(datOK$pep_var_mod, pattern = "ethyl") > 0
+datOK$MethylBool[is.na(datOK$MethylBool)] <- FALSE
+table(datOK$MethylBool)
+#
+datOK$pep_var_mod[datOK$MethylBool]
+# table(datOK$rawFile[datOK$MethylBool])
+# > table(datOK$rawFile[datOK$MethylBool])
+# 
+# 20221202_010_S433299_75773 
+# 213
+
+str(datOK)
